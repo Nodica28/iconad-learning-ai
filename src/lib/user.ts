80b66c18c16bc2bce9bd3485975012142a2e9c87
Nicdao, Justine A <justinenicdao05@gmail.com>
2024-10-11 03:42:03 +0800

    feat: added login and registration page

---
+++ src/lib/user.ts
@@ -0,0 +1,43 @@
+import clientPromise from "@/lib/mongodb";
+
+export const login = async (email: string) => {
+  try {
+    let user;
+    const client = await clientPromise;
+    const db = client.db("userDB");
+    const collection = db.collection("users");
+
+    let existingUser = await collection.findOne({ email });
+
+    if (!existingUser) {
+      user = { authenticated: false };
+    } else {
+      user = { ...existingUser, authenticated: true };
+    }
+
+    return user;
+  } catch (error) {
+    console.error("Error checking email in database:", error);
+    throw error;
+  }
+};
+
+export const register = async (email: string): Promise<void> => {
+  try {
+    const client = await clientPromise;
+    const db = client.db("userDB");
+    const collection = db.collection("users");
+
+    // Check if the email already exists
+    const existingUser = await collection.findOne({ email });
+    if (existingUser) {
+      throw new Error("User already exists.");
+    }
+
+    // Create a new user
+    await collection.insertOne({ email });
+  } catch (error) {
+    console.error("Error registering user:", error);
+    throw error;
+  }
+};
