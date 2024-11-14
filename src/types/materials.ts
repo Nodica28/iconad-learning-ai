36e753f4bb25aed4b4435622ecb1af035d1cc28c
Nicdao, Justine A <justinenicdao05@gmail.com>
2024-11-14 19:45:41 +0800

    feat: add additional fields to edit on edit learning material

--- src/types/materials.ts
+++ src/types/materials.ts
@@ -2,8 +2,12 @@ export type LearningMaterial = {
   _id: string;
   title: string;
   age_group: string[];
+  developmental_areas: string[];
+  learning_objective_tags: string[];
+  interest_tags: string[];
   content_type: string[];
-  skill_level: string;
+  engagement_level: string[];
   duration: string;
+  skill_level: string;
   document_link: string;
 };
